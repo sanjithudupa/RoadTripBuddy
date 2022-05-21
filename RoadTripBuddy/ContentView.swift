91f48b27a654ac0998c36a7da059730acdec085b
Sanjith Udupa <sanjith.udupa@gmail.com>
2022-05-21 10:33:11 -0400

    Initial Commit

---
+++ RoadTripBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoadTripBuddy
+//
+//  Created by Sanjith Udupa on 5/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RoadTripBuddy/RoadTripBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadTripBuddyApp.swift
+//  RoadTripBuddy
+//
+//  Created by Sanjith Udupa on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RoadTripBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
